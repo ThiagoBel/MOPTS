e89beecabd4efd0e17730ceae8b70e40cd55da78
Thiago Bel <narbnooxiinn@gmail.com>
2026-01-29 15:03:48 -0300

    Add files via upload

--- src/MOPTS/example.cpp
+++ src/MOPTS/example.cpp
@@ -1,55 +1,51 @@
 #include <iostream>
-#include "MOPTS.h" // Biblioteca de menu interativo no console (APENAS WINDOWS)
+#include "MOPTS.h" // biblioteca
 
-void OPTION1()
+void OPTION1(const std::string &classe) // funcao 1
 {
-    std::cout << "Opcao 1 selecionada" << std::endl;
+    std::cout << classe << " selecionada" << std::endl;
 }
-void OPTION2()
+
+void OPTION2(const std::string &classe) // funcao 2
 {
-    std::cout << "Opcao 2 selecionada" << std::endl;
+    std::cout << classe << " selecionada" << std::endl;
 }
-void OPTION3()
+
+void OPTION3(const std::string &classe) // funcao 3
 {
-    std::cout << "Opcao 3 selecionada" << std::endl;
+    std::cout << classe << " selecionada" << std::endl;
 }
 
 int main()
 {
+    MOPTS::color = true;
     // Ativa o uso de cores na opção selecionada
     // true  = fundo branco e texto preto
     // false = sem destaque de cor
-    MOPTS::color = true;
-
+    MOPTS::all_color_line = true;
     // Define se a linha inteira da opção ficará destacada
     // true  = a linha inteira é colorida
     // false = apenas o texto da opção é colorido
-
-    MOPTS::all_color_line = true;
-
+    MOPTS::clear_opts = true;
     // Define se o console será limpo após selecionar uma opção
     // true  = limpa o console antes de executar a função
     // false = mantém o menu visível
 
-    MOPTS::clear_opts = false;
-    // Lista de opções do menu
-    // Cada opção possui:
-    // - Texto que será exibido no menu
-    // - Função que será executada ao selecionar
     MOPTS::MenuOption opcoes[] = {
-        {"Opcao 1", OPTION1}, // Executa OPTION1()
-        {"Opcao 2", OPTION2}, // Executa OPTION2()
-        {"Opcao 3", OPTION3}, // Executa OPTION3()
+        // opcoes
+        {"Opcao 1", "CLASS1", OPTION1},
+        {"Opcao 2", "CLASS2", OPTION2},
+        {"Opcao 3", "CLASS3", OPTION3},
     };
+    // 1. "Opcao X" -> opção
+    // 2. "CLASS X" -> classe (informacao que manda para funcao)
+    // 3. "OPTIONX" -> funcao que executa
 
-    // Exibe o menu no console
+    MOPTS::ShowMenu("Descricao de cima", opcoes, "", "Descricao de baixo"); // mostrar no terminal
 
-    // 1º "Descricao de cima"  -> texto exibido antes das opções
-    // 2º opcoes               -> lista de opções do menu
-    // 3º ""                   -> marcador da opção selecionada (vazio = sem marcador)
-    // 4º "Descricao de baixo" -> texto exibido após as opções
-
-    MOPTS::ShowMenu("Descricao de cima", opcoes, "", "Descricao de baixo");
-
+    // 1. "Descricao de cima"  -> texto exibido antes das opções (vazio = sem descricao)
+    // 2. opcoes               -> lista de opções do menu
+    // 3. ""                   -> marcador da opção selecionada (vazio = sem marcador)
+    // 4. "Descricao de baixo" -> texto exibido após as opções (vazio = sem descricao)
     return 0;
 }
